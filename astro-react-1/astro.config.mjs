9290e1f14b7a6dec2403145176f217d2c9757a72
YAEGASHI Takeshi <yaegashi@gmail.com>
2024-05-17 02:57:49 +0000

    astro-react-1: Add json-server

--- astro-react-1/astro.config.mjs
+++ astro-react-1/astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
+        '/assets': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      }
+    }
+  }
 });
